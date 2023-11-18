c654ae92aa19cd905d0567ef47adf3f851407f1d
Bruno Oliveira <99514519+brunoaxlrose@users.noreply.github.com>
2023-11-17 21:44:05 -0300

    JBM PDV
    PDV JBM

---
+++ src/java/model/Vendas.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+/**
+ *
+ * @author Bruno Lucas, Jhonatan Nascimento, Maikon Sposito
+ */
+public class Vendas {
+
+    // Atributos
+    private int id;
+    private LocalDate data_venda;
+    private double valorTotal;
+
+    // Construtor com parâmetros
+    public Vendas(int id, Date data_venda) {
+        this.id = id;
+        this.data_venda = data_venda.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+
+    // Construtor padrão
+    public Vendas() {
+        
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getDataVenda() {
+        return this.data_venda;
+    }
+
+    public void setDataVenda(LocalDate data_venda) {
+        this.data_venda = data_venda;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+}
